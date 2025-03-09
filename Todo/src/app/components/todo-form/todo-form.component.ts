7ea5a7640f5eb3b681e169b1bc1182e7a54eae7a
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-03-09 16:52:13 +0300

    add undo functionality to task management and update UI components

--- Todo/src/app/components/todo-form/todo-form.component.ts
+++ Todo/src/app/components/todo-form/todo-form.component.ts
@@ -1,9 +1,8 @@
-
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { addTask, deleteTask, completeTask } from '../../store/task.actions';
+import { addTask, deleteTask, completeTask, undoTask } from '../../store/task.actions';
 import { TaskState, Task } from '../../store/task.reducer';
 import { selectAllTasks } from '../../store/task.selectors';
 import { CommonModule } from '@angular/common';
@@ -24,36 +23,31 @@ export class TodoFormComponent {
   tasks$: Observable<Task[]>; // Observable to get tasks from the store
 
   constructor(private fb: FormBuilder, private store: Store<TaskState>) {
-    // Initialize the form
     this.todoForm = this.fb.group({
-      // Form control with validation
-      taskName: ['', Validators.required] 
+      taskName: ['', Validators.required]
     });
-
-    // Get tasks from the store
     this.tasks$ = this.store.select(selectAllTasks);
   }
 
-  // Add a new task
   onSubmit() {
-    if (this.todoForm.valid) {
-      const newTask: Task = {
-        id: Date.now(), // Unique ID for the task
-        name: this.todoForm.value.taskName,
-        completed: false
-      };
-      this.store.dispatch(addTask({ task: newTask })); 
-      this.todoForm.reset(); // Reset the form after submission
-    }
+    const task: Task = {
+      id: Date.now(),
+      name: this.todoForm.value.taskName,
+      completed: false
+    };
+    this.store.dispatch(addTask({ task }));
+    this.todoForm.reset();
   }
 
-  // Mark a task as completed
   onComplete(id: number) {
-    this.store.dispatch(completeTask({ id })); // complete task
+    this.store.dispatch(completeTask({ id }));
   }
 
-  // Delete a task
   onDelete(id: number) {
-    this.store.dispatch(deleteTask({ id })); // delete task
+    this.store.dispatch(deleteTask({ id }));
+  }
+
+  onUndo() {
+    this.store.dispatch(undoTask());
   }
 }
--- Todo/src/app/store/task.actions.ts
+++ Todo/src/app/store/task.actions.ts
@@ -4,3 +4,4 @@ import { Task } from './task.reducer';
 export const addTask = createAction('[Task] Add Task', props<{ task: Task }>());
 export const deleteTask = createAction('[Task] Delete Task', props<{ id: number }>());
 export const completeTask = createAction('[Task] Complete Task', props<{ id: number }>());
+export const undoTask = createAction('[Task] Undo Task');
